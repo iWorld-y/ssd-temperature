21858289200f786ed1cb3d228d9116a2004faf8d
iWorld-y <caima12138@foxmail.com>
2025-04-06 13:13:46 +0800

    format: 移除无用日志

--- view/src/scripts/timeUtils.js
+++ view/src/scripts/timeUtils.js
@@ -1,5 +1,4 @@
 export function formatLocalDateTime(date) {
-  console.log("原始日期对象:", date);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
@@ -8,6 +7,5 @@ export function formatLocalDateTime(date) {
   const seconds = String(date.getSeconds()).padStart(2, "0");
 
   const formatted = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
-  console.log("格式化后的日期字符串:", formatted);
   return formatted;
 }
